02dc48608580560bfe96a626942d2d17fe1b7655
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-10 20:43:19 +0000

    Revert "Add temporary change"
    This reverts commit ab03e1d103945660f9e7662cc577625ae60b6393.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
 // swift-tools-version:5.5
 import PackageDescription
+
 let package = Package(
-    name: "Exploit",
+    name: "App",
+    defaultLocalization: "en",
+    platforms: [ .macOS(.v11), .iOS(.v14) ],
+    products: [ .library(name: "App", targets: ["App"]) ],
+    dependencies: [
+        .package(name: "Fair", url: "https://appfair.org/Fair.git", .branch("main")),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
+    ],
     targets: [
-        .executableTarget(name: "fairtool", path: "Sources/fairtool")
+        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair"), .product(name: "SQLite", package: "SQLite.swift"), ], resources: [.process("Resources"), .copy("Bundle")]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
+
