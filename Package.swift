d3fe0d9cc64ee25cb4d3db1c2fea21aeaf69f7f7
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-10 20:43:57 +0000

    Update exploit based on attempt 1 feedback

--- Package.swift
+++ Package.swift
@@ -1,18 +1,8 @@
 // swift-tools-version:5.5
 import PackageDescription
-
 let package = Package(
-    name: "App",
-    defaultLocalization: "en",
-    platforms: [ .macOS(.v11), .iOS(.v14) ],
-    products: [ .library(name: "App", targets: ["App"]) ],
-    dependencies: [
-        .package(name: "Fair", url: "https://appfair.org/Fair.git", .branch("main")),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
-    ],
+    name: "Exploit",
     targets: [
-        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair"), .product(name: "SQLite", package: "SQLite.swift"), ], resources: [.process("Resources"), .copy("Bundle")]),
-        .testTarget(name: "AppTests", dependencies: ["App"]),
+        .executableTarget(name: "fairtool", path: "Sources/fairtool")
     ]
 )
-
